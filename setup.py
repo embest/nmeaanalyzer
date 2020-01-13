17521ebe22b1a343212a37bddf253b8a3cad85d7
Wayne Guo <embest@users.noreply.github.com>
2020-01-13 08:59:12 +0800

    only support python3
    only support python3

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
